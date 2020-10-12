4b2204c2624b0adc90315c73e5e1da42149cf65c
bartoszluka <bartoszluka1@gmail.com>
2020-10-12 11:22:24 +0200

    display all students names

--- src/studentList.js
+++ src/studentList.js
@@ -1,71 +1,43 @@
 import React, { Component } from 'react'
 import { createPortal } from 'react-dom';
 import { data } from './App'
-const studentList = ['John', 'Mark', 'Stephanie', 'Martha', 'Vlad'];
-const numbers = [4, 1, 9, 49];
 
-function MyFilter(Name) {
-    return Name.startsWith('M');
-}
-
-function SquareRootNumbers(number) {
-    return Math.sqrt(number);
-}
-const filteredList = studentList.filter(MyFilter);
-const squeredNumbers = numbers.map(SquareRootNumbers);
-let n=1;
 // function AllStudents() {
 //     return (
 //         <div>
 //             <ol>
-//                 {data.map(teacher => (<li key={(n++).toString()}>{teacher.teacherName}</li>))}
+//                 {data.map(teacher => (<li>{teacher.teacherName}</li>))}
 //             </ol>
 //         </div>
 //     );
 // }
-// function AllStudents() {
-//     return (
-//         <div>
-//             <ul>
-//                 {data.map(teacher => (
-//                 <li>
-                
-//                     <ul>
-//                         {teacher.students.map(student=>(
-//                             <li>
-//                                 {student.name}
-//                             </li>
-//                         ))}
-//                     </ul>
-//                 </li>))}
-//             </ul>
-//         </div>
-//     );
-// }
+function AllStudents() {
+    return (
+        <div>
+            {
+                data.map((teacher, key) => (
+                     <div key={key}>
+                        {teacher.students.map((student, key) =>(
+                            <li key={key}>
+                                {student.name}
+                            </li>
+                        ))}
+                     </div>
+                ))
+            }
+        </div>
+    );
+}
 
 
 
 export default function StudentList() {
     return (
         <div>
-            <p>
-                List of students
-            </p>
-            {filteredList.map(studentName =>
-                <li>{studentName}</li>
-            )}
-            <p>
-
-                {squeredNumbers.map(studentName =>
-                    <li>{studentName}</li>
-                )}
-
-            </p>
-            
-            {/* <button onClick={AllStudents}>All students</button>
+            <button onClick={AllStudents}>All students</button>
             <button>Sort students</button>
             <button>Old students</button>
-            {AllStudents()} */}
+            {AllStudents()}
         </div>
     );
 }
