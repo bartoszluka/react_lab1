423850e51d4379b7870ac7b55d071cd2c37a00d2
bartoszluka <bartoszluka1@gmail.com>
2020-10-13 14:23:36 +0200

    3 arrays displayed

--- src/studentList.js
+++ src/studentList.js
@@ -10,7 +10,7 @@ function AllStudents() {
                     <div key={key}>
                         {teacher.students.map((student, key) => (
                             <li key={key}>
-                                <span>{student.name} </span>
+                                {student.name}
                             </li>
                         ))}
                     </div>
@@ -20,13 +20,40 @@ function AllStudents() {
     );
 }
 
+function DisplayStudent(props){
+    return(
+        <div>
+            <span>{props.name} {props.age}</span>
+        </div>
+    );
+}
 export default function StudentList() {
+    const tmp = data.map(teacher => teacher.students);
+    const allStudents = [].concat.apply([], tmp);
+
+    function CompareAge(s1,s2){
+        if(s1.name>s2.name) return 1;
+        if(s1.name<s2.name) return -1;
+        return 0;
+    }
+    const sortedStudents = [...allStudents];
+    sortedStudents.sort(CompareAge);
+
+    function oldStudentsFilter(num){
+        return num>20;
+    }
+    const oldStudents = allStudents.filter((student) => oldStudentsFilter(student.age));
+
     return (
         <div>
             <button onClick={AllStudents}>All students</button>
             <button>Sort students</button>
             <button>Old students</button>
-            {AllStudents()}
+            {allStudents.map(student => DisplayStudent(student))}
+            <hr/>
+            {sortedStudents.map(student => DisplayStudent(student))}
+            <hr/>
+            {oldStudents.map(student => DisplayStudent(student))}
         </div>
     );
 }
