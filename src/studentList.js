8e5edb1b981083ab180182f4c71fde4bce14eb21
bartoszluka <72729880+bartoszluka@users.noreply.github.com>
2020-11-03 20:38:57 +0100

    added missing files

--- src/studentList.js
+++ src/studentList.js
@@ -20,40 +20,53 @@ function AllStudents() {
     );
 }
 
-function DisplayStudent(props){
-    return(
+function DisplayStudent(props) {
+    return (
         <div>
             <span>{props.name} {props.age}</span>
         </div>
     );
 }
-export default function StudentList() {
-    const tmp = data.map(teacher => teacher.students);
-    const allStudents = [].concat.apply([], tmp);
 
-    function CompareAge(s1,s2){
-        if(s1.name>s2.name) return 1;
-        if(s1.name<s2.name) return -1;
+const getAllStudentNamesFromActiveTeachers = (data) => {
+
+    const reduced = data.reduce((accumulator, teacher) => {
+        return teacher.active ? [...accumulator, ...teacher.students] : [...accumulator];
+    }, []);
+    reduced.forEach(value => console.log(value));
+    return reduced;
+}
+export default function StudentList() {
+
+    // const tmp = data.map(teacher => teacher.students);
+    // const allStudents = [].concat.apply([], tmp);
+    const allStudents = getAllStudentNamesFromActiveTeachers(data);
+
+    function CompareAge(s1, s2) {
+        if (s1.name > s2.name) return 1;
+        if (s1.name < s2.name) return -1;
         return 0;
     }
     const sortedStudents = [...allStudents];
     sortedStudents.sort(CompareAge);
 
-    function oldStudentsFilter(num){
-        return num>20;
+    function oldStudentsFilter(num) {
+        return num > 20;
     }
     const oldStudents = allStudents.filter((student) => oldStudentsFilter(student.age));
 
     return (
         <div>
-            <button onClick={AllStudents}>All students</button>
+            <button onClick={() => allStudents.map(student => DisplayStudent(student))}>
+                All students
+            </button>
             <button>Sort students</button>
             <button>Old students</button>
-            {allStudents.map(student => DisplayStudent(student))}
-            <hr/>
+            {/* {allStudents.map(student => DisplayStudent(student))}
+            <hr />
             {sortedStudents.map(student => DisplayStudent(student))}
-            <hr/>
-            {oldStudents.map(student => DisplayStudent(student))}
+            <hr />
+            {oldStudents.map(student => DisplayStudent(student))} */}
         </div>
     );
 }
